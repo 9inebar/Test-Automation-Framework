eac65aac89172fed4800f47579469017f374518b
9inebar <9inebar@gmail.com>
2023-09-29 15:55:45 +0300

    Epam Leadership Test

---
+++ Epam.TestAutomation.Web/PageObjects/Panels/HeaderBlock.cs
@@ -0,0 +1,15 @@
+using Epam.TestAutomation.Core.Elements;
+using OpenQA.Selenium;
+
+namespace Epam.TestAutomation.Web.PageObjects.Panels;
+
+public class HeaderBlock : BasePanel
+{
+    public HtmlElement aboutButton => new HtmlElement(By.XPath("//a[@ href='/about']//parent::span")); 
+    public HtmlElement Leadership => new HtmlElement(By.XPath("//a[@href='https://www.epam.com/about/who-we-are/leadership' and descendant::span[contains(text(), 'Leadership')]]"));
+
+    public HeaderBlock(By locator) : base(locator)
+    {
+        
+    }
+}
